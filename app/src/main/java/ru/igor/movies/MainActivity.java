2525065fc48b2b5d92cc193f667c522a870b09fc
Ansis K <ansis.kalnyn@yandex.ru>
2025-03-02 22:09:43 +0300

    обработчик-слушатель завершения и начала потока для прогресс бара

--- app/src/main/java/ru/igor/movies/MainActivity.java
+++ app/src/main/java/ru/igor/movies/MainActivity.java
@@ -2,11 +2,14 @@ package ru.igor.movies;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,13 +17,14 @@ public class MainActivity extends AppCompatActivity {
 
     private MainViewModel viewmodel;
     private RecyclerView recyclerViewMovies;
+    private ProgressBar progressbarloading;
     private MoviesAdapter moviesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerViewMovies = findViewById(R.id.recyclerViewMovies);
+        initViews();
         moviesAdapter = new MoviesAdapter();
         recyclerViewMovies.setAdapter(moviesAdapter);
         recyclerViewMovies.setLayoutManager(new GridLayoutManager(this, 2));
@@ -33,13 +37,24 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        viewmodel.loadMovies();
+
+        viewmodel.getIsLoading().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean isLoading) {
+                progressbarloading.setVisibility(isLoading ? View.VISIBLE : View.GONE);
+            }
+        });
         moviesAdapter.setOnReachEndListener(new MoviesAdapter.OnReachEndListener() {
             @Override
             public void onReachEnd() {
                 viewmodel.loadMovies();
             }
         });
+    }
+
+    private void initViews() {
+        recyclerViewMovies = findViewById(R.id.recyclerViewMovies);
+        progressbarloading = findViewById(R.id.progressbarloading);
     }
 
 }
--- app/src/main/java/ru/igor/movies/MainViewModel.java
+++ app/src/main/java/ru/igor/movies/MainViewModel.java
@@ -8,6 +8,7 @@ import androidx.lifecycle.MutableLiveData;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import org.jetbrains.annotations.NotNull;
@@ -17,11 +18,17 @@ import java.util.List;
 public class MainViewModel extends AndroidViewModel {
     private static final String LOG_TAG = "MainViewModell";
     private final MutableLiveData<List<Movie>> moviesMLD = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> isLoading = new MutableLiveData<>(false);
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
     private int page = 1;
 
     public MainViewModel(@NotNull Application application) {
         super(application);
+        loadMovies(); //будет вызываться только при создании активити, а не при повороте экрана.
+    }
+
+    public LiveData<Boolean> getIsLoading() {
+        return isLoading;
     }
 
     public LiveData<List<Movie>> getMoviesMLD() {
@@ -30,9 +37,25 @@ public class MainViewModel extends AndroidViewModel {
 
     public void loadMovies() {
 
+        Boolean loading = isLoading.getValue();
+        if (loading != null && loading) {
+            return;
+        }
         Disposable disposable = ApiFactory.apiService.loadMovies(page)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(new Consumer<Disposable>() {
+                    @Override
+                    public void accept(Disposable disposable) throws Throwable {
+                        isLoading.setValue(true);
+                    }
+                })
+                .doAfterTerminate(new Action() {
+                    @Override
+                    public void run() throws Throwable {
+                        isLoading.setValue(false);
+                    }
+                })
                 .subscribe(new Consumer<MovieResponse>() {
                     @Override
                     public void accept(MovieResponse movieResponse) throws Throwable {
@@ -43,6 +66,7 @@ public class MainViewModel extends AndroidViewModel {
                         } else {
                             moviesMLD.setValue(movieResponse.getMovies());
                         }
+                        Log.d(LOG_TAG, "Loaded - " + String.valueOf(page));
                         page++;
                     }
                 }, new Consumer<Throwable>() {
