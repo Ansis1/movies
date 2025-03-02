1f348f9463258e11b3bcdbe81537c9f9fa7cd173
Ansis K <ansis.kalnyn@yandex.ru>
2025-03-02 22:08:27 +0300

    кол-во рез-тов в методе get

--- app/src/main/java/ru/igor/movies/ApiService.java
+++ app/src/main/java/ru/igor/movies/ApiService.java
@@ -9,7 +9,7 @@ public interface ApiService {
     String X_API_KEY = "X-API-KEY:GBKGD9T-CWK49HW-QS5WDW6-GBQW6JB";
     String ACCEPT_TYPE = "accept: application/json";
 
-    @GET("movie?limit=5&sortField=votes.kp&sortType=-1&type=movie&rating.kp=7-10")
+    @GET("movie?limit=20&sortField=votes.kp&sortType=-1&type=movie&rating.kp=7-10")
     // @GET("movie?limit=100&sortField=votes.kp&sortType=1&type=movie&rating.kp=4-8")
     @Headers({
             ACCEPT_TYPE,
