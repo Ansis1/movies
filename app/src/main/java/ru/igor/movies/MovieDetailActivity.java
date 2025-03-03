3b8180ba32b012f1b4427cefd68ed6b7c70b036e
Ansis K <ansis.kalnyn@yandex.ru>
2025-03-04 01:08:37 +0300

    MVVM for MovieDetailView

--- app/src/main/java/ru/igor/movies/MovieDetailActivity.java
+++ app/src/main/java/ru/igor/movies/MovieDetailActivity.java
@@ -3,25 +3,38 @@ package ru.igor.movies;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
+
+import java.util.List;
 
 public class MovieDetailActivity extends AppCompatActivity {
 
     private static final String EXTRA_MOVIE = "movie";
+    private static final String LOG_TAG = "MovieDetailActivity";
     private ImageView imageviewPoster;
     private TextView textViewTitle;
     private TextView textViewYear;
     private TextView textViewDescription;
+    private MovieDetailViewModel viewModel;
+    private RecyclerView recyclerViewTrailers;
+    private TrailerAdapter trailerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.v(LOG_TAG, "onCreate() MovieDetailActivity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+        viewModel = new ViewModelProvider(this).get(MovieDetailViewModel.class);
         initViews();
+        trailerAdapter = new TrailerAdapter();
+        recyclerViewTrailers.setAdapter(trailerAdapter);
         Movie movie = (Movie) getIntent().getSerializableExtra(EXTRA_MOVIE);
         Glide.with(this)
                 .load(movie.getPoster().getUrl())
@@ -30,6 +43,20 @@ public class MovieDetailActivity extends AppCompatActivity {
         textViewTitle.setText(movie.getName());
         textViewYear.setText(String.valueOf(movie.getYear()));
         textViewDescription.setText(movie.getDescription());
+        viewModel.loadTrailers(movie.getId());
+        viewModel.getTrailers().observe(this, new Observer<List<Trailer>>() {
+            @Override
+            public void onChanged(List<Trailer> trailers) {
+                Log.v(LOG_TAG, "onChanged()  getTrailers() NEW= " + trailers.toString());
+                trailerAdapter.setTrailers(trailers);
+            }
+        });
+        trailerAdapter.setOnTrailerClickListener(new TrailerAdapter.OnTrailerClickListener() {
+            @Override
+            public void onTrailerClick(String url) {
+
+            }
+        });
     }
 
     private void initViews() {
@@ -37,10 +64,11 @@ public class MovieDetailActivity extends AppCompatActivity {
         textViewTitle = findViewById(R.id.textViewTitle);
         textViewYear = findViewById(R.id.textViewYear);
         textViewDescription = findViewById(R.id.textViewDescription);
+        recyclerViewTrailers = findViewById(R.id.trailersRecyclerView);
     }
 
     public static Intent newIntent(Context context, Movie movie) {
-
+        Log.v(LOG_TAG, "newIntent MovieDetailActivity = " + movie.toString());
         Intent intent = new Intent(context, MovieDetailActivity.class);
         intent.putExtra(EXTRA_MOVIE, movie);
         return intent;
---
+++ app/src/main/java/ru/igor/movies/MovieDetailViewModel.java
@@ -0,0 +1,84 @@
+package ru.igor.movies;
+
+import android.app.Application;
+import android.util.Log;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.List;
+
+public class MovieDetailViewModel extends AndroidViewModel {
+
+    private static final CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private static final String LOG_TAG = "MovieDetailViewModel";
+    private MutableLiveData<List<Trailer>> trailers = new MutableLiveData<>();
+
+    public LiveData<List<Trailer>> getTrailers() {
+        return trailers;
+    }
+
+    public MovieDetailViewModel(@NotNull Application application) {
+        super(application);
+    }
+
+    public void loadTrailers(int id) {
+        Log.v(LOG_TAG, "init loadTrailers id= " + id);
+
+        compositeDisposable.add(
+                ApiFactory.apiService.loadTrailers(id)
+                        .subscribeOn(Schedulers.io())
+                        .doOnError(new Consumer<Throwable>() {
+                            @Override
+                            public void accept(Throwable throwable) throws Throwable {
+                                Log.d(LOG_TAG, "loadTrailers - doOnError..." + throwable.toString());
+
+                            }
+                        })
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .map(new Function<TrailerResponse, List<Trailer>>() { //преобразование типа
+                            @Override
+                            public List<Trailer> apply(TrailerResponse trailerResponse) throws Throwable {
+                                Log.v(LOG_TAG, "apply map is null= " + (trailerResponse == null));
+
+                                return trailerResponse.getTrailersList().getTrailers();
+                            }
+                        })
+
+                        .doOnSubscribe(new Consumer<Disposable>() {
+                            @Override
+                            public void accept(Disposable disposable) throws Throwable {
+                                Log.d(LOG_TAG, " loadTrailers - Starting load trailers...");
+                            }
+                        })
+                        .subscribe(new Consumer<List<Trailer>>() {
+                            @Override
+                            public void accept(List<Trailer> trailerList) {
+                                trailers.setValue(trailerList);
+                                Log.d(LOG_TAG, "loadTrailers - Success - trailers get.");
+
+                            }
+                        }, new Consumer<Throwable>() {
+                            @Override
+                            public void accept(Throwable throwable) throws Throwable {
+                                Log.d(LOG_TAG, "loadTrailers - ERROR" + throwable.toString());
+
+                            }
+                        }));
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        Log.d(LOG_TAG, "Clear view model detail");
+        compositeDisposable.dispose();
+    }
+}
