51a93b8e864e05f1af6fbba67b66d7e56591519d
Ansis K <ansis.kalnyn@yandex.ru>
2025-03-04 01:06:03 +0300

    add request logging

--- app/src/main/java/ru/igor/movies/ApiFactory.java
+++ app/src/main/java/ru/igor/movies/ApiFactory.java
@@ -1,16 +1,33 @@
 package ru.igor.movies;
 
-import io.reactivex.rxjava3.core.Single;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import java.util.concurrent.TimeUnit;
+
 public class ApiFactory {
     private final static String BASE_URL = "https://api.kinopoisk.dev/v1.4/";
+
+    private static OkHttpClient getclient() { //логирование запроса.
+
+        HttpLoggingInterceptor logger = new HttpLoggingInterceptor();
+        logger.setLevel(HttpLoggingInterceptor.Level.BODY);
+        return new OkHttpClient.Builder()
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .addInterceptor(logger)
+                .build();
+
+    }
+
     private static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .client(getclient())
             .build();
 
 
